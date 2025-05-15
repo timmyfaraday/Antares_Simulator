d740440f237d7abfcb8e6843c2941fe80a2fa1a6
Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>
2025-05-15 14:36:57 +0200

    [ANT-2140] MustRun clusters taken into account in BC (#2690)
    ## MustRun clusters contribute to BC RHS
    
    Add mustrun clusters RHS to BC RHS
    
    ## Move prepareBindingConstraint
    
    Move prepareBindingConstraint in dedicated file to reduce
    sim_calcul_economique.cpp size
    
    ---------
    
    Co-authored-by: guilpier-code <62292552+guilpier-code@users.noreply.github.com>

--- src/solver/simulation/include/antares/solver/simulation/sim_structure_probleme_economique.h
+++ src/solver/simulation/include/antares/solver/simulation/sim_structure_probleme_economique.h
@@ -152,7 +152,6 @@ struct CONTRAINTES_COUPLANTES
 
     std::vector<double> SecondMembreDeLaContrainteCouplante;
 
-    int NombreDElementsDansLaContrainteCouplante;
     int NombreDInterconnexionsDansLaContrainteCouplante;
 
     std::vector<double> PoidsDeLInterconnexion;
