d6f6d07661ff8137d0425cabdc5fcda7250a7e7a
Florian Omnès <florian.omnes@rte-france.com>
2025-07-01 17:00:31 +0200

    Remove year batches, launch jobs continually [ANT-2139] (#2718)
    ## Basic
    - [X] Loop on years, instead of looping on batches
    - [X] Implement `NumSpaceManager` mechanism, where a job requests an
    available `numSpace` at the start and returns it at the end of its
    execution
    - [X] The code builds
    - [X] No segfault when running with e.g `--force-parallel=8`
    
    ## Restore stuff
    - [X] Restore year-by-year vartiables (mc-ind directory)
    - [x] Restore synthesis variables for areas/links/clusters/etc. (mc-all
    directory)
    - [x] Restore synthesis variables for districts
    - [X] Restore random generation
    - [x] Restore `computeAnnualCostsStatistics` (should be easy)
    - [x] Restore the progression display (for Antares Web & the Legacy GUI)
    => AntaresWeb parses the logs to estimate the progression
    - [X] Restore Xpansion files single export #2726
    
    ## Result comparison
    - [x] Compare (named) MPS files
    - [X] Compare results mc-ind
    - [x] Compare results mc-all
    - [x] Check behavior with year filters (playlist)
    - [ ] windows results mc-all
    
    ## Performance comparison
    - [X] **Check performances for a real study**
    - [x] Compare the memory peak on a real large study
    
    ## Pre-requisites
    - [x] Remove in-memory TS generation and refresh
    
    ## Cleaning
    - [ ]  Activate parallel tests in CI for extended tests
    
    ---------
    
    Co-authored-by: Vincent Payet <vincent.payet@rte-france.com>
    Co-authored-by: payetvin <113102157+payetvin@users.noreply.github.com>
    Co-authored-by: Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>

--- src/solver/optimisation/opt_optimisation_lineaire.cpp
+++ src/solver/optimisation/opt_optimisation_lineaire.cpp
@@ -19,6 +19,8 @@
  * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
  */
 
+#include <mutex>
+
 #include <antares/logs/logs.h>
 #include "antares/solver/optimisation/LinearProblemMatrix.h"
 #include "antares/solver/optimisation/constraints/constraint_builder_utils.h"
@@ -31,6 +33,8 @@
 
 using namespace Antares::Solver;
 using Antares::Solver::Optimization::OptimizationOptions;
+
+std::once_flag export_once;
 
 namespace
 {
@@ -259,9 +263,11 @@ bool OPT_OptimisationLineaire(const OptimizationOptions& options,
     resizeProbleme(problemeHebdo->ProblemeAResoudre.get(),
                    problemeHebdo->ProblemeAResoudre->NombreDeVariables,
                    problemeHebdo->ProblemeAResoudre->NombreDeContraintes);
-    if (problemeHebdo->ExportStructure && problemeHebdo->firstWeekOfSimulation)
+    if (problemeHebdo->ExportStructure)
     {
-        OPT_ExportStructures(problemeHebdo, writer);
+        std::call_once(export_once,
+                       [&problemeHebdo, &writer]()
+                       { OPT_ExportStructures(problemeHebdo, writer); });
     }
 
     bool ret = runWeeklyOptimization(options.firstOptimOptions,
