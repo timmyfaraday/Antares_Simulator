5128d94ca694bcf14aabaaf454832f90abdb9549
Florian Omnès <florian.omnes@rte-france.com>
2025-08-13 11:40:14 +0200

    Revert "Add optional overflow variable for short-term storage" (#2963)

--- src/solver/optimisation/opt_construction_variables_optimisees_lineaire.cpp
+++ src/solver/optimisation/opt_construction_variables_optimisees_lineaire.cpp
@@ -138,16 +138,6 @@ void OPT_ConstruireLaListeDesVariablesOptimiseesDuProblemeLineaire(PROBLEME_HEBD
                                                                           storage.name);
                     ++NombreDeVariables;
                 }
-                // 6. Overflow
-                if (storage.allowOverflow)
-                {
-                    variableManager.ShortTermStorageOverflow(clusterGlobalIndex, pdt)
-                      = NombreDeVariables;
-                    ProblemeAResoudre->TypeDeVariable[NombreDeVariables]
-                      = VARIABLE_BORNEE_INFERIEUREMENT;
-                    variableNamer.ShortTermStorageOverflow(NombreDeVariables, storage.name);
-                    NombreDeVariables++;
-                }
             }
 
             variableManager.PositiveUnsuppliedEnergy(pays, pdt) = NombreDeVariables;
